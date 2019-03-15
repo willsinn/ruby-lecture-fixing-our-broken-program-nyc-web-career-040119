fa94e50f02fcae4912eb8183530ba2fda3d96a0b
w.h.sinn <w.h.sinn@gmail.com>
2019-03-15 09:21:27 +0000

    Automatically backed up by Learn

--- spec/a_working_program_spec.rb
+++ spec/a_working_program_spec.rb
@@ -1,6 +1,6 @@
 describe 'A working program' do
 
-  it 'not to raise an error when loaded' do
+  it 'to raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
     }.to_not raise_error
